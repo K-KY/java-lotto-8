55160bec2af9f8b162b9d761df63d461196e58e2
K-KY <kyuyoungk@naver.com>
2025-11-03 23:33:45 +0900

    feat : 생성된 로또 번호를 출력하기 위해 Ticket에 toString 추가

--- src/main/java/lotto/service/Ticket.java
+++ src/main/java/lotto/service/Ticket.java
@@ -4,6 +4,7 @@ import lotto.Lotto;
 import lotto.service.utils.LimitedList;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Ticket {
     private static final String GREATER_THAN_ORDER = "[ERROR] 구매한 갯수보다 많습니다. 갯수 : ";
@@ -32,5 +33,13 @@ public class Ticket {
         return getGames().stream()
                 .filter(g -> g.numberOfContains(lotto) >= MINIMUM_PRIZE)
                 .toList();
+    }
+
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Game game : getGames()) {
+            joiner.add(game.toString());
+        }
+        return joiner.toString();
     }
 }
