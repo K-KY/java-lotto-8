9cbd739b6cc852e5215a647feab7a9dac36d31b2
K-KY <kyuyoungk@naver.com>
2025-11-03 23:53:59 +0900

    style : 불필요한 주석 삭제

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -25,15 +25,6 @@ public class LottoService {
         return ticket;
     }
 
-    /*
-    * 3개 일치 (5,000원) - 1개
-    4개 일치 (50,000원) - 0개
-    5개 일치 (1,500,000원) - 0개
-    5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-    6개 일치 (2,000,000,000원) - 0개
-    총 수익률은 62.5%입니다.
-
-    * */
     public Statistics calculateResult(ListMap<Integer, Game> sortedGames, int bonusBall) {
         Statistics statistics = new Statistics();
         for (int i = 3; i <= 6; i++) {
