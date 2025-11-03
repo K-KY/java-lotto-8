6ff6f6ba875d0f50ba94ec7a3351d3e504760ed4
K-KY <kyuyoungk@naver.com>
2025-11-03 23:42:04 +0900

    feat : 애플리케이션 실행 메서드 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+
+import lotto.view.UserView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        UserView instance = UserView.getInstance();
+        instance.inputPurchaseInfo();
     }
 }
